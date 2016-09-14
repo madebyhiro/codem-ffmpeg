69da2b664bf22a05a35d7b99f9913f5fe747963c
Sjoerd Tieleman <sjoerd@madebyhiro.com>
2016-09-14 14:38:14 +0200

    Allow configurable ffmpeg binary.

--- spec/FFmpegSpec.js
+++ spec/FFmpegSpec.js
@@ -1,4 +1,4 @@
-describe('FFmpeg', () => {
+describe('FFmpeg creation', () => {
   const FFmpeg = require('../src/codem-ffmpeg')
 
   it('should accept an array of arguments', () => {
@@ -15,5 +15,23 @@ describe('FFmpeg', () => {
   it('should assign the arguments', () => {
     let instance = new FFmpeg(['foo', 'bar'])
     expect(instance._args).toEqual(['foo', 'bar'])
+  })
+})
+
+describe('FFmpeg spawning', () => {
+  const FFmpeg = require('../src/codem-ffmpeg')
+
+  it('should spawn the default FFmpeg', () => {
+    const child_process = new FFmpeg([]).spawn()
+    expect(child_process.spawnfile).toEqual('ffmpeg')
+  })
+  
+  describe('with a different FFmpeg', () => {
+    it('should spawn the correct FFmpeg', () => {
+      const instance = new FFmpeg([])
+      instance.ffmpegBinary = '/usr/local/no/such/ffmpeg'
+      const child_process = instance.spawn()
+      expect(child_process.spawnfile).toEqual(instance.ffmpegBinary)
+    })
   })
 })
--- src/codem-ffmpeg.js
+++ src/codem-ffmpeg.js
@@ -1,6 +1,10 @@
 const EventEmitter = require('events')
+const spawn        = require('child_process').spawn
 
 class FFmpeg extends EventEmitter {
+  get ffmpegBinary ()          { return this._ffmpegBinary || 'ffmpeg' }
+  set ffmpegBinary (newBinary) { this._ffmpegBinary = newBinary }
+  
   constructor(args) {
     if (!Array.isArray(args)) throw new Error("Expected arguments to be an array")
     
@@ -9,6 +13,7 @@ class FFmpeg extends EventEmitter {
   }
   
   spawn() {
+    return spawn(this.ffmpegBinary, this._args)
   }
 }
 
